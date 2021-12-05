5eb4974016ee749f3658e83a4c4d3cbeeadfe0b2
pczheng <zhengpc@geowit.cn>
2021-12-05 13:25:13 +0800

    add mui

--- leaflet/src/components/application/downloadpoi/index.tsx
+++ leaflet/src/components/application/downloadpoi/index.tsx
@@ -1,3 +1,7 @@
+
 export function DownloadPoi() {
-    return <div></div>
+
+    return <div>
+
+    </div>
 }
--- leaflet/src/components/myRoutes.tsx
+++ leaflet/src/components/myRoutes.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { DownloadPoi } from "./application/downloadpoi";
 import { Map } from "./map"
 
 export function MyRoutes() {
     return <Router>
         <Routes>
             <Route path="/" element={<Map />}>
+            </Route>
+            <Route path="/poi" element={<DownloadPoi />}>
             </Route>
         </Routes>
     </Router>
