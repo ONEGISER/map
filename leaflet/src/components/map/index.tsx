ad4b5dd775d4c29bc6fed0af1efb868fada3f292
pczheng <zhengpc@geowit.cn>
2021-12-23 09:52:18 +0800

    temp save

--- leaflet/src/components/map/index.tsx
+++ leaflet/src/components/map/index.tsx
@@ -3,43 +3,48 @@ import { useEffect, useState } from 'react';
 import L, { LatLng } from "leaflet"
 import axios from "axios";
 import logo from "./img/logo.png"
-import { AutoComplete, Input } from "antd";
+import { AutoComplete, Col, Input, Row } from "antd";
 import "antd/dist/antd.css"
 interface QueryResults {
     type: "route" | "station",
     name: string
     data: any
 }
+let map: L.Map
+let queryShowLayer: L.GeoJSON
+const weight = 4
+const colors: { [key: string]: any } = {
+    '轨道交通1号线': { color: "#3080b7", weight, },
+    '轨道交通2号线': { color: "#eb81b9", weight, },
+    '轨道交通3号线': { color: "#dac17d", weight, },
+    '轨道交通4号线': { color: "#86b81c", weight, },
+    '轨道交通5号线': { color: "#0000ff", weight, },
+    '轨道交通6号线': { color: "#018237", weight, },
+    '轨道交通7号线': { color: "#ee782e", weight, },
+    '轨道交通8号线': { color: "#99adac", weight, },
+    '轨道交通11号线': { color: "#fcd600", weight, },
+    '轨道交通21号线(阳逻线)': { color: "#d10195", weight },
+}
 export const Map = () => {
     const [routerData, setRouterData] = useState<any>(undefined);
     const [stationData, setStationData] = useState<any>(undefined);
-    const [queryResults, setQueryResults] = useState<QueryResults[]>([]);
-
+    const [routeResults, setRouteResults] = useState<QueryResults[]>([]);
+    const [stationResults, setStationResults] = useState<QueryResults[]>([]);
     const routerName = "路线"
     const statitonName = "站点"
+    function getDIVContent(properties: any) {
+        return `<div style="font-weight:bold;">${properties.LineName}</div>始发站：${properties.From}<br/>终点站：${properties.To}`;
+    }
 
     useEffect(() => {
         getDatas((routerData: any, stationData: any) => {
             const routerLayer = L.geoJSON(routerData, {
                 style: (feature: any) => {
-                    const weight = 4
-                    const fill = false
-                    switch (feature.properties.LineName) {
-                        case '轨道交通1号线': return { color: "#3080b7", weight, fill };
-                        case '轨道交通2号线': return { color: "#eb81b9", weight, fill };
-                        case '轨道交通3号线': return { color: "#dac17d", weight, fill };
-                        case '轨道交通4号线': return { color: "#86b81c", weight, fill };
-                        case '轨道交通5号线': return { color: "#0000ff", weight, fill };
-                        case '轨道交通6号线': return { color: "#018237", weight, fill };
-                        case '轨道交通7号线': return { color: "#ee782e", weight, fill };
-                        case '轨道交通8号线': return { color: "#99adac", weight, fill };
-                        case '轨道交通11号线': return { color: "#fcd600", weight, fill };
-                        case '轨道交通21号线(阳逻线)': return { color: "#d10195", weight, fill };
-                    }
+                    return colors[feature.properties.LineName]
                 }
             } as any).bindPopup(function (layer: any) {
                 const properties = layer.feature.properties
-                return `<div style="font-weight:bold;">${properties.LineName}</div>始发站：${properties.From}<br/>终点站：${properties.To}`;
+                return getDIVContent(properties)
             })
             const stationDataLayer = L.geoJSON(stationData, {
                 pointToLayer: (feature: any, latlng: LatLng) => {
@@ -92,7 +97,7 @@ export const Map = () => {
         const subdomains = ["0", "1", "2", "3", "4", "5", "6", "7"]
         const key = Keys.tdt
         const attribution = '&copy; <a href="https://www.tianditu.gov.cn/">天地图</a> 提供地图'
-        const opacity = 1
+        const opacity = 0.5
         const imgLayer = L.tileLayer("http://t{s}.tianditu.com/img_c/wmts?layer=img&style=default&tilematrixset=c&Service=WMTS&Request=GetTile&Version=1.0.0&Format=tiles&TileMatrix={z}&TileCol={x}&TileRow={y}&tk=" + key, {
             maxZoom: 17,
             tileSize: 256,
@@ -131,7 +136,7 @@ export const Map = () => {
 
         const defaultLayers = L.layerGroup(layers)
 
-        const map = L.map('map', {
+        map = L.map('map', {
             crs: L.CRS.EPSG4326,
             zoomControl: false,
             attributionControl: true,
@@ -156,71 +161,189 @@ export const Map = () => {
     }
 
     function handleSearch(value: string) {
-        const datas: QueryResults[] = []
-        if (stationData && stationData.features) {
+        const routerR: QueryResults[] = []
+        if (value && routerData && routerData.features) {
+            for (let i in routerData.features) {
+                const feature = routerData.features[i]
+                if (feature.properties.LineName?.indexOf(value) > -1) {
+                    routerR.push({
+                        type: "route",
+                        data: feature,
+                        name: feature.properties.LineName
+                    })
+                }
+            }
+        }
+        setRouteResults(routerR)
+
+        const stationR: QueryResults[] = []
+        if (value && stationData && stationData.features) {
             for (let i in stationData.features) {
                 const feature = stationData.features[i]
                 if (feature.properties.StationNam?.indexOf(value) > -1) {
-                    datas.push({
+                    stationR.push({
                         type: "station",
                         data: feature,
                         name: feature.properties.StationNam
                     })
                 }
             }
+
         }
-        setQueryResults(datas)
+        setStationResults(stationR)
+        if (!value) {
+            queryShowLayer?.remove()
+        }
+    }
+
+    function onSelect(value: string, option: any) {
+        queryShowLayer?.remove()
+        if (option?.data?.data) {
+            const feature = option?.data?.data
+            if (feature) {
+                if (option.data.type === "station") {
+                    queryShowLayer = L.geoJSON(
+                        {
+                            "type": "FeatureCollection",
+                            "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                            "features": [feature]
+                        } as any, {
+                            pointToLayer: (feature: any, latlng: LatLng) => {
+                                const label = String(feature.properties.StationNam)
+                                return new L.CircleMarker(latlng, {
+                                    radius: 5,
+                                    color: "#00FFFF",
+                                }).bindTooltip(label, { opacity: 1, direction: "top", permanent: true, }).openTooltip()
+                                // const label = String(feature.properties.StationNam) // Must convert to string, .bindTooltip can't use straight 'feature.properties.attribute'
+                                //.bindTooltip(label, { permanent: true, opacity: 0.7 }).openTooltip();
+                            }
+                        } as any)
+                    map.addLayer(queryShowLayer)
+                    map.flyToBounds(queryShowLayer.getBounds())
+                } else if (option.data.type === "route") {
+                    //添加查询图层
+                    queryShowLayer = L.geoJSON()
+                    map.addLayer(queryShowLayer)
+                    queryShowLayer.addData(feature)
+                    queryShowLayer.setStyle({
+                        color: "#00FFFF",
+                        weight: 5
+                    })
+                    queryShowLayer.bindPopup(getDIVContent(feature.properties)).openPopup()
+                    map.flyToBounds(queryShowLayer.getBounds())
+                }
+            }
+        }
     }
 
 
     function renderOptions() {
-        return queryResults.map((data) => {
-            return {
-                value: data.name,
-                label: (
-                    <div
-                        style={{
-                            display: 'flex',
-                            justifyContent: 'space-between',
-                        }}
-                    >
-                        <span>
-                            {/* Found {query} on{' '} */}
-                            <a
-                                // href={`https://s.taobao.com/search?q=${query}`}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                {data.name}
-                            </a>
-                        </span>
-                        {/* <span>{getRandomInt(200, 100)} results</span> */}
-                    </div>
-                ),
-            };
-        });
+        const options: any[] = []
+        const routers = {
+            label: "路线",
+            options: routeResults.map((data, i) => {
+                return {
+                    value: data.name,
+                    data,
+                    label: (
+                        <div
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                            }}
+                        >
+                            <span>
+                                <a
+
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    {data.name}
+                                </a>
+                            </span>
+                            <span>{data.data.properties.From}-{data.data.properties.To}</span>
+                        </div>
+                    ),
+                };
+            })
+        }
+        if (routeResults.length > 0) {
+            options.push(routers)
+        }
+        const stations = {
+            label: "站点",
+            options: stationResults.map((data, i) => {
+                return {
+                    value: data.name,
+                    data,
+                    label: (
+                        <div
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                            }}
+                        >
+                            <span>
+                                <a
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    {data.name}
+                                </a>
+                            </span>
+                        </div>
+                    ),
+                };
+            })
+        }
+        if (stationResults.length > 0) {
+            options.push(stations)
+        }
+        return options
     }
 
-
+    function renderLegend() {
+        const backgroundColor = `rgba(255,255,255,0.85)`
+        const jsxs = []
+        for (let i in colors) {
+            const jsx = <Row justify="center" align="middle" style={{ padding: 10 }}>
+                <Col>
+                    <Row justify="center">
+                        <div style={{ width: 30, height: 5, background: colors[i].color, paddingRight: 20 }}></div>
+                    </Row>
+                    <Row justify="center" style={{ textAlign: "center" }}>
+                        {i}
+                    </Row>
+                </Col>
+            </Row>
+            jsxs.push(jsx)
+        }
+        return <Row justify="center" style={{ backgroundColor }}>
+            {jsxs}
+        </Row>
+    }
 
     return <div style={{ height: "100%", width: "100%", position: "absolute" }}>
         <div className="lmap" id="map">
 
         </div>
-        <div style={{ position: "absolute", left: 10, top: 10 }}>
+        <div style={{ position: "absolute", left: 10, top: 10, zIndex: 1000 }}>
             <img style={{ height: 100, zIndex: 10 }} src={logo} />
         </div>
-        <div style={{ position: "absolute", right: 80, top: 10 }}>
+        <div style={{ position: "absolute", right: 80, top: 10, zIndex: 1000 }}>
             <AutoComplete
                 dropdownMatchSelectWidth={252}
-                style={{ width: 340, height: 44 }}
+                style={{ width: 300, height: 44 }}
                 options={renderOptions()}
-                // onSelect={onSelect}
+                onSelect={onSelect}
                 onSearch={handleSearch}
             >
 
-                <Input.Search style={{ height: "100%" }} bordered size="large" placeholder="请输入关键词查询地铁线路或站点" enterButton />
+                <Input.Search style={{ height: "100%" }} bordered size="large" placeholder="请输入地铁线路或站点名称" enterButton />
             </AutoComplete>
         </div>
+        <Row justify="center" style={{ position: "absolute", width: "100%", bottom: 10, zIndex: 1000 }}>
+            {renderLegend()}
+        </Row>
     </div>
 };
