53fb47a437bd5f8fa1baf166d770a546e28927b9
onegiser <1305780485@qq.com>
2023-08-16 10:59:20 +0800

    修改服务

--- cesium/src/components/map.tsx
+++ cesium/src/components/map.tsx
@@ -22,41 +22,8 @@ export const Map = () => {
     // const attribution = '&copy; <a href="https://gsjlxkgc.com/">甘肃记录小康工程</a> 提供数据'
     // const dom = document.createElement("div")
     // dom.innerHTML = attribution
-    let wmts = new Cesium.WebMapTileServiceImageryProvider({
-      url: "http://60.13.54.71:31607/cim-platform/api/sharelink/getwmts?service=wmts&format=image/png&layer=cim_platform_20230602164907452514009128632320&key=76cceb49752ca62e80bc767e5485a1f9&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol=-{TileCol}",
-      layer: "cim_platform_20230602164907452514009128632320", 
-      style: "",
-      format: "image/png",
-      tileMatrixSetID: "EPSG:4326", //坐标系
-      tilingScheme: new GeographicTilingScheme(),
-      tileMatrixLabels: [
-        "EPSG:4326:0",
-        "EPSG:4326:1",
-        "EPSG:4326:2",
-        "EPSG:4326:3",
-        "EPSG:4326:4",
-        "EPSG:4326:5",
-        "EPSG:4326:6",
-        "EPSG:4326:7",
-        "EPSG:4326:8",
-        "EPSG:4326:9",
-        "EPSG:4326:10",
-        "EPSG:4326:11",
-        "EPSG:4326:12",
-        "EPSG:4326:13",
-        "EPSG:4326:14",
-        "EPSG:4326:15",
-        "EPSG:4326:16",
-        "EPSG:4326:17",
-        "EPSG:4326:18",
-      ], //查看geoserver，看切了几层
-      maximumLevel: 19, //设置最高显示层级
-      //    rectangle: new Cesium.Rectangle.fromDegrees(
-      //       73.499,
-      //       3.837,
-      //       135.087,
-      //       53.561
-      //     ),
+    let wmts = new Cesium.ArcGisMapServerImageryProvider({
+      url: "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer",
     });
 
     const viewer = new Viewer("map", {
